f940a92378c77f5c98e0d2d2f2483c3b2508663a
Mashiro <adadam@qq.com>
2017-10-01 11:30:04 +0800

    Update scroll-line.js

--- js/scroll-line/scroll-line.js
+++ js/scroll-line/scroll-line.js
@@ -6,4 +6,9 @@ var wintop = $(window).scrollTop(), docheight =
  			var scrolled = (wintop/(docheight-winheight))*100;
   
   		$('.scroll-line').css('width', (scrolled + '%'));
+    
+    $('.goTOP').each(function () {
+        var gotop = scrolled.toFixed(0)
+        $(this).html(gotop + '% <i class="fa fa-arrow-up" aria-hidden="true"></i>');
+    });
 });
